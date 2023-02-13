7f183a30d0b0f5e1c6bd17f05e8deced34e99d8e
Jaumees <60508054+Jaumees@users.noreply.github.com>
2023-02-13 19:40:07 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,5 +16,6 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git passwall-luci https://github.com/xiaorouji/openwrt-passwall/tree/luci' >>feeds.conf.default
 echo 'src-git mosdns https://github.com/QiuSimons/openwrt-mos' >>feeds.conf.default
 
